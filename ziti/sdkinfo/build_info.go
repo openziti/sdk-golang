a3a5ebb5c5df1bdb7123e245b143c2d4cb5c78da
ziti-ci <ziti-ci@netfoundry.io>
2020-05-26 21:34:58 +0000

    Release v0.11.40

--- ziti/sdkinfo/build_info.go
+++ ziti/sdkinfo/build_info.go
@@ -3,9 +3,9 @@
 package sdkinfo
 
 const (
-	Version   = "v0.9.0"
-	Revision  = "a8baa9923e52"
+	Version   = "v0.11.40"
+	Revision  = "b39fa3fc8627"
 	Branch    = "master"
-	BuildUser = "Travis CI"
-	BuildDate = "2020-02-10 11:38:37"
+	BuildUser = "Travis CI User"
+	BuildDate = "2020-05-26 21:34:58"
 )
