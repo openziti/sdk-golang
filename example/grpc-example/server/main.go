33acc6ac3c6dc1f9762256b33b85aea069f69035
Eugene K <eugene.kobyakov@netfoundry.io>
2022-06-27 16:37:01 -0400

    add grpc sample based on https://github.com/grpc/grpc-go/tree/master/examples/helloworld

---
+++ example/grpc-example/server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/openziti/sdk-golang/ziti"
+	"github.com/openziti/sdk-golang/ziti/config"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"log"
+)
+
+var (
+	identity = flag.String("identity", "", "Ziti Identity file")
+	service  = flag.String("service", "", "Ziti Service")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	cfg, err := config.NewFromFile(*identity)
+	if err != nil {
+		log.Fatalf("failed to load ziti identity{%s}", identity)
+	}
+
+	ztx := ziti.NewContextWithConfig(cfg)
+
+	lis, err := ztx.Listen(*service)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
